4b76bbaaf0fe7ce6b716c7546f56e7c94f029844
mkzmohanad <mkzmohanad@gmail.com>
2025-02-18 18:10:34 +0200

    backend-video

--- Backend/Routes/VideoRoutes.js
+++ Backend/Routes/VideoRoutes.js
@@ -1,34 +1,14 @@
 const express = require("express");
 
 const { protectRoutes } = require("../Controllers/AuthController");
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
 const { uploadingVideo, deleteVideo } = require("../Controllers/VideoController");
 // import { uploadingVideo } from "../Controllers/VideoController"
-=======
->>>>>>> a43efcc82a81e657e93089fdf0ba24a4b9825071
-=======
->>>>>>> a43efcc82a81e657e93089fdf0ba24a4b9825071
->>>>>>> e466617 (again)
 
 const VideoRoutes = express.Router();
 
 VideoRoutes.use(protectRoutes)
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
 VideoRoutes.route("/uploadVideo").post(uploadingVideo)
 VideoRoutes.route("/deleteVideo").delete(deleteVideo)
-
-=======
->>>>>>> a43efcc82a81e657e93089fdf0ba24a4b9825071
-=======
->>>>>>> a43efcc82a81e657e93089fdf0ba24a4b9825071
->>>>>>> e466617 (again)
-
 
 module.exports = VideoRoutes;
