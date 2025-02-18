7e6ad5361cc79dc98ced86c60e3b639d2204299e
mkzmohanad <mkzmohanad@gmail.com>
2025-02-18 18:12:46 +0200

    backend-video-dubbing

--- Backend/Models/VideoModel.js
+++ Backend/Models/VideoModel.js
@@ -4,22 +4,9 @@ const videoSchema = mongoose.Schema({
     userID: {
         type : mongoose.Schema.ObjectId,
         ref : "User",
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
     },
     originalVideo: {
         type : String,
-        required : true,
-    },
-    dubbedVideo: {
-        type:String,
-    },
-=======
-=======
->>>>>>> a43efcc82a81e657e93089fdf0ba24a4b9825071
->>>>>>> e466617 (again)
         required : true,
     },
     videoURL: {
@@ -34,13 +21,6 @@ const videoSchema = mongoose.Schema({
         enum: ['pending', 'processing', 'completed', 'failed'],
         default: 'pending'
     },
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> a43efcc82a81e657e93089fdf0ba24a4b9825071
-=======
->>>>>>> a43efcc82a81e657e93089fdf0ba24a4b9825071
->>>>>>> e466617 (again)
     createdAt: {
         type: Date,
         default: Date.now(),
